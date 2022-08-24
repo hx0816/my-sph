3162cbf5e515cb6ec3f23d89f9975c43ee04de02
hx0816 <879164392@qq.com>
2022-08-24 12:06:07 +0800

    获取search数据

--- src/api/index.js
+++ src/api/index.js
@@ -2,16 +2,38 @@ import request from './request'
 import mockRequest from './mock'
 
 // 三级联动数据接口 /api/product/getBaseCategoryList GET
-export const reqCategoryList = ()=>{
-    return request('/product/getBaseCategoryList',{
-        methods:'get'
+export const reqCategoryList = () => {
+    return request('/product/getBaseCategoryList', {
+        methods: 'get'
     })
 }
 
-export const reqBannerList = ()=>{
-    return mockRequest('/banner',{methods:'get'})
+// 首页轮播图数据
+export const reqBannerList = () => {
+    return mockRequest('/banner', { methods: 'get' })
 }
 
-export const reqFloorList = ()=>{
+//首页floor数据
+export const reqFloorList = () => {
     return mockRequest.get('/floor')
+}
+
+// search数据 /api/list  POST
+// 可选参数
+// {
+//     "category3Id": "61",
+//     "categoryName": "手机",
+//     "keyword": "小米",
+//     "order": "1:desc",
+//     "pageNo": 1,
+//     "pageSize": 10,
+//     "props": ["1:1700-2799:价格", "2:6.65-6.74英寸:屏幕尺寸"],
+//     "trademark": "4:小米"
+//   }
+export const reqSearchInfo = (data={}) =>{
+    return request({
+        url:'/list',
+        method:'post',
+        data
+    })
 }
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,6 +1,20 @@
-const state = {}
-const actions = {}
-const mutations = {}
+import {reqSearchInfo} from '@/api'
+
+
+const state = {
+    searchInfo:{}
+}
+const actions = {
+   async searchInfo({commit}){
+    const data = await reqSearchInfo()
+    commit('SEARCHINFO',data.data)
+    }
+}
+const mutations = {
+    SEARCHINFO(state,searchInfo){
+        state.searchInfo = searchInfo
+    }
+}
 const getters = {}
 
 export default {
